951aaed0014216a40e97db1d9b9f63fd5fa91405
ceshi1 <haha>
2023-07-30 18:00:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @ClassName GitTest
+ * @Author Mr. Zheng
+ * @Date 2023/7/30 17:59
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
